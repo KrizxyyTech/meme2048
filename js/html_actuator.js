5c50c6634241d63f8d2709c11752e5cc0d992fbd
Geoff Stearns <geoff@deconcept.com>
2014-04-07 22:18:32 -0400

    fix score share stuff

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -155,13 +155,12 @@ HTMLActuator.prototype.scoreTweetButton = function () {
   var tweet = document.createElement("a");
   tweet.classList.add("twitter-share-button");
   tweet.setAttribute("href", "https://twitter.com/share");
-  tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
-  tweet.setAttribute("data-counturl", "http://gabrielecirulli.github.io/2048/");
+  tweet.setAttribute("data-via", "tensafefrogs");
+  tweet.setAttribute("data-url", "http://tensafefrogs.github.io/2048");
+  tweet.setAttribute("data-counturl", "http://tensafefrogs.github.io/2048/");
   tweet.textContent = "Tweet";
 
-  var text = "I scored " + this.score + " points at 2048, a game where you " +
-             "join numbers to score high! #2048game";
+  var text = "I mashed memes together to score " + this.score + " points! A winner is me!";
   tweet.setAttribute("data-text", text);
 
   return tweet;
